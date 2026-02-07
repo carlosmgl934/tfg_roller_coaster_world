4be9b126a914570dbda92ad364490906936edd1a
carlosmgl934 <carlosmgl934@gmail.com>
2026-02-07 18:42:43 +0100

    simple cambio de estructura de carpetas
    he cambiado todos los .html por .php y he creado un par de html separados

---
+++ RollerCoasterWorld/web/views/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'structure/header.php';
+?>
+
+<main>
+    <h1>Bienvenido a RollerCoaster World</h1>
+    <p>La mejor base de datos de montañas rusas</p>
+</main>
+
+<?php
+require_once 'structure/footer.php';
+?>
---
+++ RollerCoasterWorld/web/views/structure/header.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+
+<body>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="profile.php">Mi perfil</a></li>
+        <li><a href="rollercoasters.php">Montañas Rusas</a></li>
+        <li><a href="parks.php">Parques</a></li>
+        <li><a href="forums.php">Foros</a></li>
+      </ul>
+    </nav>
+    <div class="user-actions">
+      <a href="login.php">Iniciar sesión</a>
+      <a href="register.php">Registrarse</a>
+    </div>
+  </header>
