b753c3662a2d6a62b44f09cf7c333587fff461de
carlosmgl934 <carlosmgl934@gmail.com>
2026-02-09 16:47:03 +0100

    Creacion de formulario de contacto y su correspondiente validación simple de los campos más importantes
    A su vez se han creado numerosos archivos para terminar de completar la estructura del programa y una nueva tabla para los mensajes del formulario

---
+++ RollerCoasterWorld/api/database/contact_messages.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS contact_messages(
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT DEFAULT NULL,  --usuario que nos contacta (si no esta logueado, sera NULL)
+    user_name VARCHAR(100) NOT NULL,  
+    user_email VARCHAR(100) NOT NULL,  
+    subject VARCHAR(150) NOT NULL,  --asunto del mensaje
+    user_message TEXT NOT NULL,  --mensaje del usuario
+    is_read BOOLEAN DEFAULT FALSE,  
+    
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  --fecha de envio
+   
+   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL
+);
