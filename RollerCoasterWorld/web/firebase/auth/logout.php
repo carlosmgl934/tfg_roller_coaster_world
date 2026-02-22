76524ca281cd91319361666e477e675247d0ad5b
Tu Nombre <u1646291625@gmail.com>
2026-02-22 17:08:02 +0100

    Actualiza .gitignore: ignora node_modules, .env, logs y archivos sensibles

---
+++ RollerCoasterWorld/web/firebase/auth/logout.php
@@ -0,0 +1,14 @@
+<?php
+require_once '../../../web/views/structure/header.php';
+?>
+
+<main>
+  <h2>Cerrar Sesión</h2>
+  <button type="button" onclick="signOut()">Logout</button>
+</main>
+
+
+
+<?php
+require_once '../../../web/views/structure/footer.php';
+?>
